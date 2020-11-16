a17302cbe436c3215b9b650fd6286e4258d89dfe
sugino <git@sugi.cc>
2020-11-16 18:44:10 +0900

    add timeline control to vfx-fireworks

---
+++ Assets/VfxGraph/Scripts/VfxTimeCurveParam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.Timeline;
+
+public class VfxTimeCurveParam : MonoBehaviour,ITimeControl
+{
+    public VisualEffect targetVfx;
+    public string propName;
+    public AnimationCurve curve;
+
+    public void OnControlTimeStart()
+    {
+    }
+
+    public void OnControlTimeStop()
+    {
+    }
+
+    public void SetTime(double time)
+    {
+        targetVfx.SetFloat(propName, curve.Evaluate((float)time));
+    }
+}
