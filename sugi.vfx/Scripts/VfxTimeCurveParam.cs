fe722086a84ecc17169d0acc6ed93378e125b722
sugino <git@sugi.cc>
2021-03-09 16:28:05 +0900

    package化

---
+++ sugi.vfx/Scripts/VfxTimeCurveParam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.Timeline;
+
+namespace sugi.cc.vfx
+{
+    public class VfxTimeCurveParam : MonoBehaviour, ITimeControl
+    {
+        public VisualEffect targetVfx;
+        public string propName;
+        public AnimationCurve curve;
+
+        public void OnControlTimeStart()
+        {
+        }
+
+        public void OnControlTimeStop()
+        {
+        }
+
+        public void SetTime(double time)
+        {
+            targetVfx.SetFloat(propName, curve.Evaluate((float)time));
+        }
+    }
+}
---
+++ sugi.vfx/Scripts/VfxTimelineControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.VFX;
+using UnityEngine.Events;
+
+namespace sugi.cc.vfx
+{
+    public class VfxTimelineControl : MonoBehaviour, ITimeControl
+    {
+        public VisualEffect targetVfx;
+        public string timeStartEvent = "Spawn";
+        public string timeStopEvent = "Stop";
+        public bool reinitOnstart = false;
+        public bool sendStartEvent = true;
+        public bool sentStopEvent = true;
+
+        [Header("Additional Events")]
+        public UnityEvent onTimeStart;
+        public UnityEvent onTimeStop;
+        public TimeEvent onTimeSet;
+
+        void Reset()
+        {
+            var vfx = GetComponent<VisualEffect>();
+            if (vfx != null)
+                targetVfx = vfx;
+        }
+
+        public void OnControlTimeStart()
+        {
+            if (reinitOnstart)
+                targetVfx.Reinit();
+            onTimeStart.Invoke();
+            if (sendStartEvent)
+                targetVfx.SendEvent(timeStartEvent);
+        }
+
+        public void OnControlTimeStop()
+        {
+            onTimeStop.Invoke();
+            if (sendStartEvent)
+                targetVfx.SendEvent(timeStopEvent);
+        }
+
+        public void SetTime(double time)
+        {
+            onTimeSet.Invoke((float)time);
+        }
+
+        [System.Serializable]
+        public class TimeEvent : UnityEvent<float> { }
+    }
+}
